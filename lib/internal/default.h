ee0fd063e4f0054302b459bb272540ebed1712af
Apoorv Singal <apoorvsingal0@gmail.com>
2020-11-09 13:26:15 +0530

    too many things to mention

--- lib/internal/default.h
+++ lib/internal/default.h
@@ -9,4 +9,12 @@
 #define I_NEW(type, val) ({ void *ptr = malloc(sizeof(type)); type v = val; memcpy(ptr, &v, sizeof(type)); ptr; })
 #define I_NEW_NV(type) ((type *)malloc(sizeof(type)))
 
+#define RETURN(expr) return expr
+
+int v0_main();
+
+int main() {
+    return v0_main();
+};
+
 #endif /* VO_DEFAULT */
--- lib/internal/types.h
+++ lib/internal/types.h
@@ -1,24 +1,21 @@
 #ifndef VO_INTERNAL_TYPES
 #define VO_INTERNAL_TYPES
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long long u64;
+#include <stdint.h>
 
-typedef char i8;
-typedef short i16;
-typedef int i32;
-typedef long long i64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
 
 typedef float f32;
 typedef double f64;
 
-typedef u64 uptr;
 typedef u8 bool;
-
-#define true 1
-#define false 0
-#define null ((void *)0)
 
 #endif /* VO_INTERNAL_TYPES */
